809a494001ce014ab37b256b05b6c81569cf6f94
raresgafton <rares.gafton@toptrendgaming.com>
2024-02-02 16:01:53 +0200

    added cache server draft

---
+++ cacheserver/src/main/groovy/com/ttg/cache/cacheserver/CacheserverApplication.groovy
@@ -0,0 +1,13 @@
+package com.ttg.cache.cacheserver
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CacheserverApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CacheserverApplication, args)
+	}
+
+}
---
+++ cacheserver/src/test/groovy/com/ttg/cache/cacheserver/CacheserverApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.ttg.cache.cacheserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CacheserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
