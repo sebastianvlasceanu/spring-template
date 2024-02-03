4f446e1d7233dd3c7624e56cd6b7598a9b54b76a
raresgafton <rares.gafton@toptrendgaming.com>
2024-02-03 13:07:53 +0200

    Commented out tests for now

---
+++ cacheclient/src/test/java/com/ttg/cache/cacheclient/CacheclientApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.ttg.cache.cacheclient;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CacheclientApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
