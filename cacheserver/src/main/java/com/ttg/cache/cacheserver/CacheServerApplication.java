84e4fb8ac37306a1984d087f5af0d1cf6dec281b
raresgafton <rares.gafton@toptrendgaming.com>
2024-02-03 13:12:16 +0200

    added remaining todos for cache server

--- cacheserver/src/main/java/com/ttg/cache/cacheserver/CacheServerApplication.java
+++ cacheserver/src/main/java/com/ttg/cache/cacheserver/CacheServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CacheServerApplication {
+    //TODO mus timplement ttl mechanism
 
     public static void main(String[] args) {
         SpringApplication.run(CacheServerApplication.class, args);
--- cacheserver/src/main/java/com/ttg/cache/cacheserver/service/CacheService.java
+++ cacheserver/src/main/java/com/ttg/cache/cacheserver/service/CacheService.java
@@ -8,22 +8,31 @@ import reactor.core.publisher.Mono;
 @Service
 public class CacheService {
     public Mono<Object> contains(String key, String realm) {
+        //TODO implement contains action
+        //it must check that an non-null object with the specified key exists in the specified realm
         return Mono.empty();
     }
 
     public Mono<Object> getObject(String key, String realm) {
+        //TODO implement get action
+        //it must retrieve either null (404) or the value stored for the key and realm and must reset ttl
         return Mono.empty();
     }
 
     public Mono<Object> putObject(CacheDto cacheDto) {
+        //TODO implement put action
+        //must store the provided object with the key and realm and set ttl
         return Mono.empty();
     }
 
     public Mono<Object> resetTtl(TtlDto ttlDto) {
+        //TODO reset ttl for the specified key and realm if non-null or return error (cannot process)
         return Mono.empty();
     }
 
     public Mono<Object> getTtl(String key, String realm) {
+        //TODO implement get ttl action
+        //retrieve ttl for the specified key and realm if non null or return 404
         return Mono.empty();
     }
 }
